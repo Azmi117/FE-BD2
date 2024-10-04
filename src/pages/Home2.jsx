0c582252896bf820cf27f3f7ae63495fceed62cb
Pistaciox <avatarkora636@gmail.com>
2024-10-04 23:45:57 +0700

    semiFinal

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -20,15 +20,15 @@ const Home2 = () => {
         <>
             <Navbar/>
                 <div className="flex min-h-screen bg-[#F19ED2]">
-                    <div className="w-screen flex justify-between mt-20">
-                        <div className="my-60">
+                    <div className="w-screen flex justify-between mt-32">
+                        <div className="my-40">
                             <Link to={'/'} className="ms-3 text-2xl text-white hover:text-[#D63484]">Prev</Link>
                         </div>
                         <div className="flex flex-col mx-auto">
                             <h1 className="text-white text-2xl">AKU SENENG BANGET BISA NGERAYAIN ULANG TAHUN KAMU CANTIK</h1>
-                            <img src="./Mable.gif" alt="" />
+                            <img src="./Mable.gif" alt=""/>
                         </div>
-                        <div className="my-60">
+                        <div className="my-40">
                             <Link to={'/home2'} className="me-3 text-2xl text-white hover:text-[#D63484]">Next</Link>
                         </div>
                     </div>
